9f65c25e77ed0ef9f5ff1df4173e438301803cf0
Mad Max <85madmaxfury@gmail.com>
2022-07-18 19:32:52 +0530

    revised commonly used builtins

--- Basics/3 - scalar_type_float.py
+++ Basics/3 - scalar_type_float.py
@@ -1,6 +1,5 @@
-# 2. Floating Points => Adopts IEEE - 754 double
-# precision with 53-bits of binary precision with approx. 15 - 16 
-# significant digits in the decimal
+# 2. Floating Points => Adopts IEEE - 754 double precision with 53-bits
+# of binary precision with approx. 15 - 16 significant digits in the decimal
 
 float_a = 3.123 # float literal
 print(float_a)
@@ -13,6 +12,8 @@ PLANKS = 1.616e-35 # Plank's Constant
 print(PLANKS)
 
 # Call the float(number) constructor to perform the required conversion
+print(float(3), float('3')) # results as 3.0
+
 PLANKS_STRING = '1.616e-35' # Plank's Constant string representation
 conv_planks = float(PLANKS_STRING)
 print(conv_planks, type(conv_planks))
---
+++ Basics/4 - builtin_constants.py
@@ -0,0 +1,26 @@
+# 3. Constants that live in the built-in namespace
+# The below are most commonly used
+
+# 3.1 True - The true value of the bool type. 
+# Assignments to True are illegal and raise a SyntaxError.
+# That is True = anything # Is not allowed!
+
+# 3.2 False - The false value of the bool type. 
+# Assignments to False are illegal and raise a SyntaxError.
+# That is False = anything # Is not allowed!
+
+# An object can be converted into a boolean value by calling the
+# bool constructor
+print(bool(3)) # returns True
+print(bool(0), bool(0.0), bool(complex(0, 0)), bool(None)) # returns False
+print(bool(""), bool([]), bool(())) # returns False
+
+# 3.3 None - An object frequently used to represent the absence of a value,
+# as when default arguments are not passed to a function.
+# Assignments to None are illegal and raise a SyntaxError.
+# None is the sole instance of the NoneType type.
+# That is None = anything # Is not allowed!
+variable_a = None # Does nothing, Na da!!!
+
+# Note: on a Python REPL session, the _ (underscore) stores the result of
+# the previously executed statement/ expression
